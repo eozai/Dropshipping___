e33cc7803d03cb9d7ae201c63af5f0917d752270
eozai <jagadishsuthar27daggh@gmail.com>
2025-05-22 16:26:09 +0530

    Add App.js

---
+++ public/src/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ fontFamily: 'sans-serif', textAlign: 'center', padding: '2rem' }}>
+      <h1>Welcome to Your Dropshipping Store</h1>
+      <p>This is a clean and professional site. Add products and start selling!</p>
+    </div>
+  );
+}
+
+export default App;
